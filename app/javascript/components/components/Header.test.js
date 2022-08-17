361275ea691ab6d275484a482d655d075e79ce5c
Jordyn Jensen <jordyneajensen@gmail.com>
2022-08-17 15:50:10 -0700

    header finished with testing

--- app/javascript/components/components/Header.test.js
+++ app/javascript/components/components/Header.test.js
@@ -9,10 +9,11 @@ import Adapter from "enzyme-adapter-react-16";
 
 // Imports in the component we are going to be testing.
 import Header from "./Header";
-import { Nav, NavItem } from 'reactstrap'
+
 
 //Allows us to utilize the adapter we import in earlier, allowing us to call and render a component.
 Enzyme.configure({ adapter: new Adapter() });
+
 
 
 // links for all users
@@ -20,70 +21,45 @@ describe("When Header renders", () => {
   let renderedHeader;
 
   beforeEach(() => {
-    renderedHeader = shallow(<Header/>);
+    renderedHeader = shallow(<Header/>)
   })
 
   it("displays a Home link", () => {
-    const renderedHomeRoute = renderedHeader.find("[path='/']")
-    expect(renderedHomeRoute.length).toEqual(1);
+    const renderedHomeRoute = renderedHeader.find("[href='/']")
+    expect(renderedHomeRoute.length).toEqual(1)
+  })
+
+  it("displays an About link", () => {
+    const headerAboutRoute = renderedHeader.find("[href='/spotabout']")
+    expect(headerAboutRoute.length).toEqual(1)
+  })
+
+
+  it("displays a Recent Spots link", () => {
+    const headerRecentSpotsRoute = renderedHeader.find("[href='/spotabout']")
+    expect(headerRecentSpotsRoute.length).toEqual(1)
   })
 })
 
-//   it("displays an About link", () => {
-//     const headerAbout = renderedHeader.find("About");
+//links for logged in users
+  describe("When Header logged in renders", () => {
+    let renderedHeader;
+    beforeEach(() => {
+      renderedHeader = shallow(<Header logged_in={true} />)
+    })
 
-//     expect(headerAbout.length).toEqual(1);
-//   });
+    it("displays a view all spots link", () => {
+      const headerViewAllSpotsRoute = renderedHeader.find("[href='/spotindex']")
+      expect(headerViewAllSpotsRoute.length).toEqual(1)
+    })
 
-//   it("displays a Recent Spots link", () => {
-//     const headerRecentSpots = renderedHeader.find("Recent Spots");
+    it("displays a add spot link", () => {
+      const headerAddSpotRoute = renderedHeader.find("[href='/spotnew']")
+      expect(headerAddSpotRoute.length).toEqual(1);
+    })
 
-//     expect(headerRecentSpots.length).toEqual(1);
-//   });
-
-// // links for logged out users
-//   describe("when header logged out renders", () => {
-//     let renderedHeader;
-//     beforeEach(() => {
-//       renderedHeader = shallow(<Header logged_in={false} />);
-//     });
-
-//     it("displays a sign in link", () => {
-//       const headerSignIn = renderedHeader.find("Sign In");
-//       expect(headerSignIn.length).toEqual(1);
-//     });
-
-//     it("displays a sign up link", () => {
-//       const headerSignUp = renderedHeader.find("Sign Up");
-//       expect(headerSignUp.length).toEqual(1);
-//     });
-//   });
-
-// //links for logged in users
-//   describe("When Header logged in renders", () => {
-//     let renderedHeader;
-//     beforeEach(() => {
-//       renderedHeader = shallow(<Header logged_in={true} />);
-//     });
-
-//     it("displays a view all spots link", () => {
-//       const headerViewAllSpots = renderedHeader.find("View All Spots");
-//       expect(headerViewAllSpots.length).toEqual(1);
-//     });
-
-//     it("displays a add spot link", () => {
-//       const headerAddSpot = renderedHeader.find("Add Spot");
-//       expect(headerAddSpot.length).toEqual(1);
-//     });
-
-//     it("displays a sign out link", () => {
-//       const headerSignOut = renderedHeader.find("Sign Out");
-//       expect(headerSignOut.length).toEqual(1);
-//     });
-
-//     it("displays a my spots link", () => {
-//       const headerMySpots = renderedHeader.find("MySpots");
-//       expect(headerMySpots.length).toEqual(1);
-//     });
-//   });
-// });
+    it("displays a my spots link", () => {
+      const headerMySpotsRoute = renderedHeader.find("[href='/myspots']")
+      expect(headerMySpotsRoute.length).toEqual(1)
+    })
+  })
