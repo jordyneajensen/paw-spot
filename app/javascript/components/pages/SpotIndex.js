6944a8ee093611f75d482dd6da381d701bbdfb66
arnoldc94 <carnold9438@gmail.com>
2022-08-17 18:02:02 -0700

    SpotIndex.js refactored

--- app/javascript/components/pages/SpotIndex.js
+++ app/javascript/components/pages/SpotIndex.js
@@ -5,13 +5,25 @@ class SpotIndex extends Component {
     constructor(props) {
         super(props)
     }
+
+    conditionalRendering = () =>{
+        if(this.props.logged_in === false){
+            return this.props.spots.length > 3 ? this.props.spots.slice(0,3) : this.props.spots
+        }else{
+            return this.props.spots
+        }
+    }
+
   render() {
+    
+    const {logged_in, spots} = this.props
+    
     return (
       <>
-        {this.props.logged_in && 
+        {logged_in && 
             <h1>Recent Pet Spots</h1>
         }
-        {!this.props.logged_in && 
+        {!logged_in && 
             <>
                 <h1>Sneak Peak!</h1>
                 <h3>Sign Up for the full</h3>
@@ -19,22 +31,8 @@ class SpotIndex extends Component {
             </>
         }
         <Row>
-            {this.props.spots && this.props.logged_in && this.props.spots.map(spot => {
-                return(
-                    <Col sm={4} key={spot.id}>
-                        <Card >
-                            <CardImg src={spot.image} alt="Pet-friendly image"/>
-                            <CardBody>
-                                <CardTitle>{spot.name}</CardTitle>
-                                <CardSubtitle>{spot.street} {spot.city} {spot.state} {spot.zip}</CardSubtitle>
-                                <Button><a href={`/spotshow/${spot.id}`} >More info here</a></Button>
-                            </CardBody>
-                        </Card>
-                    </Col>
-                )
-            })}
-        
-            {this.props.spots && !this.props.logged_in && (this.props.spots.length > 3 ? this.props.spots.slice(0,3) : this.props.spots).map(spot => {
+
+            {spots && this.conditionalRendering().map(spot => {
                 return(
                     <Col sm={4} key={spot.id}>
                         <Card >
