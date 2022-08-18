61238af0bc035a4a4dc404b4976beae22e048d8f
arnoldc94 <carnold9438@gmail.com>
2022-08-17 21:34:11 -0700

    protected create controller, rspec, fetch, route, form, jest

--- app/javascript/components/pages/SpotNew.js
+++ app/javascript/components/pages/SpotNew.js
@@ -1,9 +1,114 @@
 import React, { Component } from 'react'
+import { Redirect } from 'react-router-dom'
+import { Button } from 'reactstrap'
+import { Form, FormGroup, Input, Label} from 'reactstrap'
 
 class SpotNew extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      newSpot: {
+        name:"",
+        street: "",
+        city: "",
+        state: "",
+        zip:"",
+        description:"",
+        image: "",
+        user_id: this.props.current_user.id
+      },
+      submitted : false
+    }
+  }
+
+  handleChange = (e) => {
+  
+    let {newSpot} = this.state
+    newSpot[e.target.name] = e.target.value
+    this.setState({newSpot: newSpot})
+  }
+  
+  handleSubmit = () => {
+    this.props.createSpot(this.state.newSpot)
+    this.setState({submitted: true})
+  }
+
   render() {
     return (
-      <div>SpotNew</div>
+      <>
+        <h1>New Pet Spot</h1>
+        <Form>
+        <FormGroup>
+              <Label for="name">Name</Label>
+                <Input
+                  type="text"
+                  name="name"
+                  onChange = {this.handleChange}
+                  value = {this.state.newSpot.name}
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="street">Street</Label>
+                <Input
+                  type="text"
+                  name="street"
+                  onChange = {this.handleChange}
+                  value = {this.state.newSpot.street}
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="city">City</Label>
+                <Input
+                  type="text"
+                  name="city"
+                  onChange = {this.handleChange}
+                  value = {this.state.newSpot.city}
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="state">State</Label>
+                <Input
+                  type="text"
+                  name="state"
+                  onChange={this.handleChange}
+                  value={this.state.newSpot.state}
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="zip">Zip</Label>
+                <Input
+                  type="text"
+                  name="zip"
+                  onChange={this.handleChange}
+                  value={this.state.newSpot.zip}
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="description">Description</Label>
+                <Input
+                  type="text"
+                  name="description"
+                  onChange={this.handleChange}
+                  value={this.state.newSpot.description}
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="image">Image</Label>
+                <Input
+                  type="text"
+                  name="image"
+                  onChange={this.handleChange}
+                  value={this.state.newSpot.image}
+              />
+            </FormGroup>
+              <Button
+                name="submit"
+                onClick={this.handleSubmit}>
+                Submit
+              </Button>
+            {   this.state.submitted  && <Redirect to="/spotindex" />}
+          </Form> 
+      </>
     )
   }
 }
--- spec/requests/spots_spec.rb
+++ spec/requests/spots_spec.rb
@@ -23,4 +23,191 @@ RSpec.describe "Spots", type: :request do
       expect(spots.length).to eq(1)
     end
   end
+
+    describe "POST /create" do
+    
+      it"creates a new listing" do
+        user = User.where(email: 'test@example.com').first_or_create(password: '12345678', password_confirmation: '12345678')
+    
+        spot_params ={
+          spot: {
+            name: "Buzz Coffee",
+            street: "123 other way",
+            city: "San Diego",
+            state: "CA",
+            zip: "22400",
+            description: "pet friendly spot one",
+            image:"https://images.hellogiggles.com/uploads/2017/06/23025549/coffeedoggettyimages-585356343-e1498237725281.jpg",
+            user_id: user.id  
+
+        }
+      }
+    
+      post '/spots', params: spot_params
+
+      spot = JSON.parse(response.body)
+      expect(response).to have_http_status(200)
+      
+      expect(spot['name']).to eq "Buzz Coffee"
+      expect(spot['street']).to eq "123 other way"
+      expect(spot['city']).to eq "San Diego"
+      expect(spot['state']).to eq "CA"
+      expect(spot['zip']).to eq "22400"
+      expect(spot['description']).to eq "pet friendly spot one"
+      expect(spot['image']).to eq "https://images.hellogiggles.com/uploads/2017/06/23025549/coffeedoggettyimages-585356343-e1498237725281.jpg"
+    end
+
+      it "doesn't create a spot without a name" do
+
+        user = User.where(email: 'test@example.com').first_or_create(password: '12345678', password_confirmation: '12345678')
+
+        spot_params = {
+          spot: {
+              street: "123 other way",
+              city: "San Diego",
+              state: "CA",
+              zip: "22400",
+              description: "pet friendly spot one",
+              image:"https://images.hellogiggles.com/uploads/2017/06/23025549/coffeedoggettyimages-585356343-e1498237725281.jpg",
+              user_id: user.id  
+          }
+        }
+  
+        post "/spots", params: spot_params
+        expect(response.status).to eq 422
+        spot = JSON.parse(response.body)
+        expect(spot['name']).to include "can't be blank" 
+      end
+
+      it "doesn't create a spot without a street" do
+
+        user = User.where(email: 'test@example.com').first_or_create(password: '12345678', password_confirmation: '12345678')
+
+        spot_params = {
+          spot: {
+              name: "Buzz Coffee",
+              city: "San Diego",
+              state: "CA",
+              zip: "22400",
+              description: "pet friendly spot one",
+              image:"https://images.hellogiggles.com/uploads/2017/06/23025549/coffeedoggettyimages-585356343-e1498237725281.jpg",
+              user_id: user.id  
+          }
+        }
+  
+        post "/spots", params: spot_params
+        expect(response.status).to eq 422
+        spot = JSON.parse(response.body)
+        expect(spot['street']).to include "can't be blank" 
+      end
+
+      it "doesn't create a spot without a city" do
+
+        user = User.where(email: 'test@example.com').first_or_create(password: '12345678', password_confirmation: '12345678')
+
+        spot_params = {
+          spot: {
+              name: "Buzz Coffee",
+              street: "123 other way",
+              state: "CA",
+              zip: "22400",
+              description: "pet friendly spot one",
+              image:"https://images.hellogiggles.com/uploads/2017/06/23025549/coffeedoggettyimages-585356343-e1498237725281.jpg",
+              user_id: user.id  
+          }
+        }
+  
+        post "/spots", params: spot_params
+        expect(response.status).to eq 422
+        spot = JSON.parse(response.body)
+        expect(spot['city']).to include "can't be blank" 
+      end
+      it "doesn't create a spot without a state" do
+
+        user = User.where(email: 'test@example.com').first_or_create(password: '12345678', password_confirmation: '12345678')
+
+        spot_params = {
+          spot: {
+              name: "Buzz Coffee",
+              street: "123 other way",
+              city: "San Diego",
+              zip: "22400",
+              description: "pet friendly spot one",
+              image:"https://images.hellogiggles.com/uploads/2017/06/23025549/coffeedoggettyimages-585356343-e1498237725281.jpg",
+              user_id: user.id  
+          }
+        }
+  
+        post "/spots", params: spot_params
+        expect(response.status).to eq 422
+        spot = JSON.parse(response.body)
+        expect(spot['state']).to include "can't be blank" 
+      end
+
+      it "doesn't create a spot without a zip" do
+
+        user = User.where(email: 'test@example.com').first_or_create(password: '12345678', password_confirmation: '12345678')
+
+        spot_params = {
+          spot: {
+              name: "Buzz Coffee",
+              street: "123 other way",
+              city: "San Diego",
+              state: "CA",
+              description: "pet friendly spot one",
+              image:"https://images.hellogiggles.com/uploads/2017/06/23025549/coffeedoggettyimages-585356343-e1498237725281.jpg",
+              user_id: user.id  
+          }
+        }
+  
+        post "/spots", params: spot_params
+        expect(response.status).to eq 422
+        spot = JSON.parse(response.body)
+        expect(spot['zip']).to include "can't be blank" 
+      end
+
+      it "doesn't create a spot without a description" do
+
+        user = User.where(email: 'test@example.com').first_or_create(password: '12345678', password_confirmation: '12345678')
+
+        spot_params = {
+          spot: {
+              name: "Buzz Coffee",
+              street: "123 other way",
+              city: "San Diego",
+              state: "CA",
+              zip: "22400",
+              image:"https://images.hellogiggles.com/uploads/2017/06/23025549/coffeedoggettyimages-585356343-e1498237725281.jpg",
+              user_id: user.id  
+          }
+        }
+  
+        post "/spots", params: spot_params
+        expect(response.status).to eq 422
+        spot = JSON.parse(response.body)
+        expect(spot['description']).to include "can't be blank" 
+      end
+
+      it "doesn't create a spot without an image " do
+
+        user = User.where(email: 'test@example.com').first_or_create(password: '12345678', password_confirmation: '12345678')
+
+        spot_params = {
+          spot: {
+              name: "Buzz Coffee",
+              street: "123 other way",
+              city: "San Diego",
+              state: "CA",
+              zip: "22400",
+              description: "pet friendly spot one",
+              user_id: user.id  
+          }
+        }
+  
+        post "/spots", params: spot_params
+        expect(response.status).to eq 422
+        spot = JSON.parse(response.body)
+        expect(spot['image']).to include "can't be blank" 
+      end     
+  end  
 end
